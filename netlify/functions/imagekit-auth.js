54fb7e23464345e9ae806adefe4e249c762543c8
Ahmed-Nox <ahmedlegendforgaming@gmail.com>
2026-06-21 04:55:27 +0300

    Add files via upload

---
+++ netlify/functions/imagekit-auth.js
@@ -0,0 +1,29 @@
+const ImageKit = require("imagekit");
+
+exports.handler = async function () {
+  try {
+    const imagekit = new ImageKit({
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+      urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+    });
+
+    const authParams = imagekit.getAuthenticationParameters();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(authParams),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
+};
