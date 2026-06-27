eb5f8cf7fd3c5b3ac5419fd681921bc63db87e4b
Ahmed-Nox <ahmedlegendforgaming@gmail.com>
2026-06-27 06:37:36 +0300

    Refactor to use Express for imagekit authentication

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
+const express = require("express");
 const ImageKit = require("imagekit");
 
-exports.handler = async function () {
-  try {
-    const imagekit = new ImageKit({
-      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-      urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-    });
+const app = express();
 
-    const authParams = imagekit.getAuthenticationParameters();
+app.get("/", (req, res) => {
+  const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  });
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify(authParams),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: error.message,
-      }),
-    };
-  }
-};
+  res.json(imagekit.getAuthenticationParameters());
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
